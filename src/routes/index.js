8bf521fe6071310e451e862b5fa39fe4cba4433c
umashankarmjabagond <umashankarjabagond@gmail.com>
2026-01-29 10:05:01 +0530

    fix:fix api end point for categories

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,6 @@ const categoryRoutes = require("./categoryRoutes/categoryRoutes");
 const router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
-router.use("/category", categoryRoutes);
+router.use("/categories", categoryRoutes);
 
 module.exports = router;
