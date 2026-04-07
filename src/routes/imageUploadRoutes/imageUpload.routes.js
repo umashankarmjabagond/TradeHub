011523a994823548e1f41b5033026ded0d6a0bc7
umashankarmjabagond <umashankarjabagond@gmail.com>
2026-04-07 11:42:38 +0530

    feat : add profile details like contact company and bank

--- src/routes/imageUploadRoutes/imageUpload.routes.js
+++ src/routes/imageUploadRoutes/imageUpload.routes.js
@@ -3,6 +3,7 @@ const {
   uploadProductImages,
   uploadCategoryImage,
   getUploadSignature,
+  uploadProfileImage,
 } = require("../../controllers/imageUpload/imageUpload.controller.js");
 const { upload } = require("../../middleware/upload.middleware.js");
 const { verifyToken } = require("../../middleware/auth.middleware.js");
@@ -13,5 +14,13 @@ const router = express.Router();
 router.post("/images", upload.array("files", 5), uploadProductImages);
 router.post("/category-image", upload.single("file"), uploadCategoryImage);
 router.get("/signature", verifyToken, getUploadSignature);
+
+//user profile route
+router.post(
+  "/profile-image",
+  verifyToken,
+  upload.single("file"),
+  uploadProfileImage,
+);
 
 module.exports = router;
